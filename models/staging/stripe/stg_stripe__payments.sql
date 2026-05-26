275734fc87f9b3b7d04af520a183d89ed58d46b7
paul.edwin.wallace@gmail.com <paul.edwin.wallace@gmail.com>
2026-05-26 13:20:28 +0000

    tweaked stripe staging names

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -13,10 +13,8 @@ renamed as (
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
-        amount as payment_amount,
-        created as payment_created,
-        _batched_at
-
+        amount/100 as payment_amount,
+        created as created_at
     from source
 
 )
