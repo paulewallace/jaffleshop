1fb422bf4a76e74a634e293b3ad3251509e8addf
paul.edwin.wallace@gmail.com <paul.edwin.wallace@gmail.com>
2026-05-26 13:11:22 +0000

    cleaned up staging models

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,16 @@
- select
-        id as customer_id,
-        first_name,
-        last_name
+WITH source as (
+select *  from {{ source('jaffle_shop', 'customers') }}
 
-    from raw.jaffle_shop.customers
+),
+
+renamed as (
+    SELECT 
+        id as customer_id
+        , first_name
+        , last_name
+
+    FROM source
+
+)
+
+SELECT * FROM renamed
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,14 @@
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    WITH 
+    source as (
+        SELECT * FROM {{ source('jaffle_shop', 'orders') }}
+    ),
+    renamed as (
+        select
+            id as order_id,
+            user_id as customer_id,
+            order_date,
+            status as order_status
+        from 
+            source
+    )
+    select * from renamed
